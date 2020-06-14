c83ebc049baf3bf454c7c8bdf60c0480b3e0c6f0
kocetora <kocetora@gmail.com>
2020-06-14 14:28:59 +0300

    ready form

---
+++ src/app/core/enums/formEnum.ts
@@ -0,0 +1,33 @@
+export class FormEnums {
+    static genders = [
+        'male',
+        'female'
+      ];
+    static grades = [
+        'primary',
+        'secondary',
+        'unfinished_higher',
+        'higher'
+      ];
+    static professions = [
+        'trainee',
+        'dealer',
+        'inspector',
+        'manager',
+        'pit_boss',
+        'waiter',
+        'barman'
+      ];
+      static messengers = [
+        'Telegram',
+        'Viber',
+        'WhatsApp'
+      ];
+      static languages = ['russian', 'english'];
+      static languageProficiency = [
+        'basic',
+        'intermediate',
+        'fluent',
+        'native'
+    ];
+}
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { CustomValidators } from '../core/validators/validator';
 import { BodyService } from '../core/services/body.service';
 import { FetchService } from '../core/services/fetch.service';
+import { atLeastOne } from '../core/validators/atLeastOne';
 
 @Component({
   selector: 'app-form',
@@ -39,10 +40,10 @@ export class FormComponent implements OnInit {
   ];
   languages = ['russian', 'english'];
   languageProficiency = [
-    'native',
-    'fluent',
+    'basic',
     'intermediate',
-    'basic'
+    'fluent',
+    'native'
   ];
 
   constructor(
@@ -53,15 +54,15 @@ export class FormComponent implements OnInit {
 
     ngOnInit() {
       this.form = this.formBuilder.group({
-        name: ['', [Validators.required, CustomValidators.noWhitespace]],
-        surname: ['', [Validators.required, CustomValidators.noWhitespace]],
+        name: ['', [Validators.required, CustomValidators.noWhitespace, Validators.maxLength(255)]],
+        surname: ['', [Validators.required, CustomValidators.noWhitespace, Validators.maxLength(255)]],
         sex: [this.genders[0], Validators.required],
         education: [this.grades[0], Validators.required],
         born: ['',  [Validators.required]],
         height: ['',  [Validators.required, Validators.min(30), Validators.max(300)]],
-        phoneNumber: ['', [Validators.required, CustomValidators.noWhitespace]],
-        email: ['', [Validators.required, Validators.email]],
-        prefferedRegion: [''],
+        phoneNumber: ['', [Validators.required, CustomValidators.noWhitespace, Validators.maxLength(255)]],
+        email: ['', [Validators.required, Validators.email, Validators.maxLength(255)]],
+        prefferedRegion: ['', Validators.maxLength(255)],
         expectedSalary: ['', [Validators.required, Validators.min(1), Validators.max(100000)]],
         note: ['', Validators.maxLength(255)],
         unemployedFor: this.formBuilder.group({
@@ -72,35 +73,32 @@ export class FormComponent implements OnInit {
           workExperienceYears: ['', [Validators.required, Validators.min(0), Validators.max(100)]],
           workExperienceMonths: ['', [Validators.required, Validators.min(0), Validators.max(11)]],
         }),
-        languageSkills: this.formBuilder.group({
+        languages: this.formBuilder.group({
           english: [],
           russian: [],
-          englishProficiency: ['basic', Validators.required],
-          russianProficiency: ['basic', Validators.required]
+        }, { validator: atLeastOne(Validators.required) }),
+        languageProficiency: this.formBuilder.group({
+          englishProficiency: [this.languageProficiency[0], Validators.required],
+          russianProficiency: [this.languageProficiency[0], Validators.required]
         }),
         professions: this.formBuilder.group({
-          trainee: [''],
-          dealer: [''],
-          inspector: [''],
-          manager: [''],
-          pit_boss: [''],
-          waiter: [''],
-          barman: [''],
-        })
+          trainee: [],
+          dealer: [],
+          inspector: [],
+          manager: [],
+          pit_boss: [],
+          waiter: [],
+          barman: [],
+        }, { validator: atLeastOne(Validators.required) }),
+        messengers: this.formBuilder.group({
+          WhatsApp: ['', Validators.maxLength(255)],
+          Telegram: ['', Validators.maxLength(255)],
+          Viber: ['', Validators.maxLength(255)],
+        }, { validator: atLeastOne(Validators.required) })
       });
     }
 
-        // messengers: new FormGroup({
-        //   WhatsApp: new FormControl(),
-        //   Telegram: new FormControl(),
-        //   Viber: new FormControl(),
-        //   msWhatsApp: new FormControl(),
-        //   msTelegram: new FormControl(),
-        //   msViber: new FormControl(),
-        // })
-
     submit() {
-
         console.log(this.form);
         // const formData = this.bodyService.convertFormData({...this.form.value});
         // this.fetchService.addForm(formData).subscribe();
