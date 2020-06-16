555faf0fc2df133d9ba82832f03296e197cbf9e6
kocetora <kocetora@gmail.com>
2020-06-16 10:42:13 +0300

    lang-switcher

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CreateFormComponent } from './create-form/create-form.component';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClientModule } from '@angular/common/http';
+import { TranslateModule } from '@ngx-translate/core';
 import { ErrorComponent } from './error/error.component';
 import { ViewModule } from './view/view.module';
 import { MaterialModule } from './shared/modules/material.module';
@@ -13,10 +12,6 @@ import { AuthModule } from './auth/auth.module';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { LangSwitcherModule } from './core/components/lang-switcher/lang-switcher.module';
-
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
-}
 
 @NgModule({
   declarations: [
@@ -27,18 +22,12 @@ export function HttpLoaderFactory(http: HttpClient) {
   imports: [
     BrowserModule,
     HttpClientModule,
+    TranslateModule,
     MaterialModule,
     ViewModule,
     FormModule,
     AuthModule,
     RouterModule.forRoot(appRoutes, { useHash: true }),
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient]
-      }
-    }),
     LangSwitcherModule
   ],
   providers: [],
--- src/app/core/components/lang-switcher/lang-switcher.module.ts
+++ src/app/core/components/lang-switcher/lang-switcher.module.ts
@@ -2,6 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../../../shared/modules/material.module';
 import { LangSwitcherComponent } from './lang-switcher.component';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -10,6 +17,13 @@ import { LangSwitcherComponent } from './lang-switcher.component';
   imports: [
     CommonModule,
     MaterialModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
   ],
   exports: [
     LangSwitcherComponent
