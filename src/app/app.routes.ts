94ba96baaddf211d2f0c9700031af2ca898d26c6
kocetora <kocetora@gmail.com>
2021-01-07 11:17:05 +0200

    chore: reset form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { CreateFormComponent } from './create-form/create-form.component';
 import { ViewComponent } from './view/view.component';
 import { ErrorComponent } from './error/error.component';
-import { AuthGuard } from './core/guards/auth.guard';
 import { RouteNames } from './shared/consts/route-names.enum';
 import { authRoutes } from './auth/auth.routes';
 
@@ -15,7 +14,6 @@ export const appRoutes: Routes = [
   {
     path: RouteNames.VIEW,
     component: ViewComponent,
-    canActivate: [AuthGuard],
   },
   {
     path: RouteNames.AUTH,
