ce7e96379ac45e6187207328b20407068099b31c
kocetora <kocetora@gmail.com>
2021-04-01 07:52:54 +0300

    test: one wrong

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -11,7 +11,7 @@ describe('workspace-project App', () => {
   it('should display welcome message', () => {
     // page.navigateTo();
     // expect(page.getTitleText()).toEqual('front app is running!');
-    expect(0).toEqual(1);
+    expect(0).toEqual(0);
   });
 
   // afterEach(async () => {
